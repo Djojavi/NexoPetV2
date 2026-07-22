80674eb5d2c0b79598ca926aa3e1229ebccbadaf
Froggotkd <djojavi19@gmail.com>
2026-07-22 13:18:05 -0500

    change gateway host

--- auth-api-service/apps/api-gateway/src/app.module.ts
+++ auth-api-service/apps/api-gateway/src/app.module.ts
@@ -12,8 +12,8 @@ import { PetsController } from './pets/pets.controller';
         name: 'AUTH_SERVICE', // Un "apodo" para inyectarlo después
         transport: Transport.TCP,
         options: {
-          host: '127.0.0.1', // O 'auth-service' si ambos estuvieran en Docker
-          port: 3001,        // El mismo puerto que configuramos en el main.ts de Auth
+          host: process.env.AUTH_SERVICE_HOST || 'auth-service',
+          port: Number(process.env.AUTH_SERVICE_PORT) || 3001,
         },
       },
       {
